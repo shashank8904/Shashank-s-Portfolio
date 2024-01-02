1186f847365b8407f93d46c963048ef3cf3376dc
shashank8904 <152384914+shashank8904@users.noreply.github.com>
2024-01-02 18:08:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// var tablinks =document.getElementsByClassName('tab-links');
+// var tabcontents =document.getElementsByClassName('tab-contents'); 
+// function opentab(tabname) 
+// {
+//     for(tablinks of tablinks){
+//         tablinks.classList.remove("active-link");
+//     }
+//     for(tabcontents of tabcontents){
+//         tabcontents.classList.remove("active-tab");
+//     }
+//    event.currentTarget.classList.add("active-link");
+//    document.getElementById('tabname').classList.add("active-tab") 
+// }
